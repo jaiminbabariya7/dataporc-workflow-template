b68234890b2382993ead29b3c1e4ec74de3dae38
jaiminbabariya7 <130003933+jaiminbabariya7@users.noreply.github.com>
2024-07-03 06:26:31 -0400

    Add files via upload

--- wordcount.py
+++ wordcount.py
@@ -1,46 +1,19 @@
 #!/usr/bin/env python3
 import sys
 
-def mapper():
+def word_count_mapper():
+    word_counts = {}
     for line in sys.stdin:
         line = line.strip()
         words = line.split()
         for word in words:
-            print(f'{word}\t1')
+            word_counts[word] = word_counts.get(word, 0) + 1
+    return word_counts
 
-def reducer():
-    current_word = None
-    current_count = 0
-    word = None
-
-    for line in sys.stdin:
-        line = line.strip()
-        word, count = line.split('\t', 1)
-        try:
-            count = int(count)
-        except ValueError:
-            continue
-
-        if current_word == word:
-            current_count += count
-        else:
-            if current_word:
-                print(f'{current_word}\t{current_count}')
-            current_count = count
-            current_word = word
-
-    if current_word == word:
-        print(f'{current_word}\t{current_count}')
+def word_count_reducer(word_counts):
+    for word, count in word_counts.items():
+        print(f'{word}\t{count}')
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python wordcount.py [mapper|reducer]")
-        sys.exit(1)
-
-    if sys.argv[1] == 'mapper':
-        mapper()
-    elif sys.argv[1] == 'reducer':
-        reducer()
-    else:
-        print("Invalid argument. Use 'mapper' or 'reducer'.")
-        sys.exit(1)
+    word_counts = word_count_mapper()
+    word_count_reducer(word_counts)
