e58ba2cd77473b5374c90bcf824bc877f9d9bf77
jaiminbabariya7 <130003933+jaiminbabariya7@users.noreply.github.com>
2024-07-03 07:10:52 -0400

    Add files via upload

--- wordcount.py
+++ wordcount.py
@@ -2,54 +2,46 @@
 import sys
 from google.cloud import storage
 
-def read_input_from_gcs(bucket_name, input_file_path):
-    """Reads input from a file stored in GCS."""
-    client = storage.Client()
-    bucket = client.bucket(bucket_name)
-    blob = bucket.blob(input_file_path)
-    return blob.download_as_text()
+def read_from_gcs(input_path):
+    """Read data from GCS."""
+    storage_client = storage.Client()
+    bucket_name, blob_name = input_path.split('/', 3)[-1].split('/', 1)
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    content = blob.download_as_string().decode('utf-8')
+    return content
 
-def write_output_to_gcs(bucket_name, output_file_path, output_data):
-    """Writes output to a file in GCS."""
-    client = storage.Client()
-    bucket = client.bucket(bucket_name)
-    blob = bucket.blob(output_file_path)
-    blob.upload_from_string(output_data)
-
-def word_count_mapper(input_data):
-    """Performs word count mapping."""
+def word_count_mapper(content):
+    """Perform word count."""
     word_counts = {}
-    for line in input_data.splitlines():
-        line = line.strip()
-        words = line.split()
+    for line in content.splitlines():
+        words = line.strip().split()
         for word in words:
             word_counts[word] = word_counts.get(word, 0) + 1
     return word_counts
 
-def word_count_reducer(word_counts):
-    """Performs word count reducing."""
-    output_data = []
-    for word, count in word_counts.items():
-        output_data.append(f'{word}\t{count}')
-    return '\n'.join(output_data)
+def write_to_gcs(output_path, word_counts):
+    """Write results to GCS."""
+    storage_client = storage.Client()
+    bucket_name, blob_name = output_path.split('/', 3)[-1].split('/', 1)
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    output_data = '\n'.join([f'{word}\t{count}' for word, count in word_counts.items()])
+    blob.upload_from_string(output_data)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: wordcount.py <bucket_name> <input_file_path> <output_file_path>")
+    if len(sys.argv) != 3:
+        print("Usage: python wordcount.py <input_gcs_path> <output_gcs_path>")
         sys.exit(1)
 
-    bucket_name = sys.argv[1]
-    input_file_path = sys.argv[2]
-    output_file_path = sys.argv[3]
+    input_gcs_path = sys.argv[1]
+    output_gcs_path = sys.argv[2]
 
-    # Read input data from GCS
-    input_data = read_input_from_gcs(bucket_name, input_file_path)
+    # Read data from GCS
+    input_data = read_from_gcs(input_gcs_path)
 
-    # Perform word count mapping
+    # Perform word count
     word_counts = word_count_mapper(input_data)
 
-    # Perform word count reducing
-    output_data = word_count_reducer(word_counts)
-
-    # Write output data to GCS
-    write_output_to_gcs(bucket_name, output_file_path, output_data)
+    # Write results to GCS
+    write_to_gcs(output_gcs_path, word_counts)
